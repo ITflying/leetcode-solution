7d035c15f7310e2dfa92acad85b9cc513a7b3edf
ITflying <442665858@qq.com>
2019-09-16 09:40:59 +0800

    [add]question 14 solved (15  may be solved)

--- way/src/_14_最长公共前缀/LongestCommonPrefix.java
+++ way/src/_14_最长公共前缀/LongestCommonPrefix.java
@@ -1,5 +1,7 @@
 package _14_最长公共前缀;
 
+import com.sun.deploy.util.StringUtils;
+
 import java.util.Arrays;
 
 /**
@@ -25,27 +27,27 @@ import java.util.Arrays;
 public class LongestCommonPrefix {
 
     public static void main(String[] args) {
-        String[] s = new String[]{"flower","flow","flight"};
+        String[] s = new String[]{"flower", "flow", "flight"};
         System.out.println(Arrays.toString(s));
-        System.out.println("最长公共前缀为："+longestCommonPrefix_02(s));
+        System.out.println("最长公共前缀为：" + longestCommonPrefix_05(s));
     }
 
     /**
      * 第一种
      */
     private static String longestCommonPrefix_01(String[] strs) {
-        if (strs.length == 0){
+        if (strs.length == 0) {
             return "";
         }
-        
+
         String ret = "";
         int i = 0;
-        while (true){
-            if (i >= strs[0].length()){
+        while (true) {
+            if (i >= strs[0].length()) {
                 return ret;
             }
             for (int j = 1; j < strs.length; j++) {
-                if (i >= strs[j].length() || strs[j].charAt(i) !=  strs[0].charAt(i)){
+                if (i >= strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i)) {
                     return ret;
                 }
             }
@@ -53,20 +55,20 @@ public class LongestCommonPrefix {
             i++;
         }
     }
-    
+
     //  region 抄作业时间
 
     /**
-     * 水平扫描法
+     * 第二种方法：水平扫描法
      */
     private static String longestCommonPrefix_02(String[] strs) {
-        if (strs.length == 0){
+        if (strs.length == 0) {
             return "";
         }
-        
+
         String prefix = strs[0];
-        for (int i = 1; i < strs.length;i++){
-            while (strs[i].indexOf(prefix) != 0){
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
                 prefix = prefix.substring(0, prefix.length() - 1);
                 if (prefix.isEmpty()) {
                     return "";
@@ -77,24 +79,107 @@ public class LongestCommonPrefix {
     }
 
     /**
-     * 水平扫描法
+     * 第三种方法：水平扫描法
+     * 这其实就是第一种方法的简化版本，用第一个字符串去匹配后面的字符串
      */
     private static String longestCommonPrefix_03(String[] strs) {
-        if (strs.length == 0){
+        if (strs == null || strs.length == 0) {
             return "";
         }
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
 
-        String prefix = strs[0];
-        for (int i = 1; i < strs.length;i++){
-            while (strs[i].indexOf(prefix) != 0){
-                prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()) {
+    private static String longestCommonPrefix_03_upgrade(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String s = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(s) != 0) {
+                s = s.substring(0, s.length() - 1);
+                if (s.isEmpty()) {
                     return "";
                 }
             }
         }
-        return prefix;
+        return s;
     }
-    
+
+    /**
+     * 第四种方法：分治
+     * 其实就是将问题拆分成两个部分，然后递归寻找左右两个字符串相似的部分
+     */
+    private static String longestCommonPrefix_04(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        return longestCommonPrefix_04_recursive(strs, 0, strs.length - 1);
+    }
+
+    private static String longestCommonPrefix_04_recursive(String[] strs, int left, int right) {
+        if (left == right) {
+            return strs[left];
+        } else {
+            int mid = (left + right) / 2;
+            String lcpLeft = longestCommonPrefix_04_recursive(strs, left, mid);
+            String lcpRight = longestCommonPrefix_04_recursive(strs, mid + 1, right);
+            return longestCommonPrefix_04_common(lcpLeft, lcpRight);
+        }
+    }
+
+    private static String longestCommonPrefix_04_common(String left, String right) {
+        int min = Math.min(left.length(), right.length());
+        for (int i = 0; i < min; i++) {
+            if (left.charAt(i) != right.charAt(i)) {
+                return left.substring(0, i);
+            }
+        }
+        return left.substring(0, min);
+    }
+
+    /**
+     * 第五种方法：二分查找法
+     * 这其实就是把一个字符串按整体的一般长度进行匹配，不断切分匹配，直到找到最长公共前缀
+     */
+    private static String longestCommonPrefix_05(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int minLen = Integer.MAX_VALUE;
+        for (String str : strs) {
+            minLen = Math.min(minLen, str.length());
+        }
+        int low = 1;
+        int high = minLen;
+        while (low <= high) {
+            int middle = (low + high) / 2;
+            if (longestCommonPrefix_05_isCommonPrefix(strs, middle)) {
+                low = middle + 1;
+            } else {
+                high = middle - 1;
+            }
+        }
+        return strs[0].substring(0, (low + high) / 2);
+    }
+
+    private static boolean longestCommonPrefix_05_isCommonPrefix(String[] strs, int len) {
+        String str1 = strs[0].substring(0, len);
+        for (int i = 1; i < strs.length; i++) {
+            if (!strs[i].startsWith(str1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
     // endregoin
 }
---
+++ way/src/_15_三数之和/SumOfThreeNum.java
@@ -0,0 +1,108 @@
+package _15_三数之和;
+
+import java.util.*;
+
+/**
+ 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+
+ 注意：答案中不可以包含重复的三元组。
+
+ 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+
+ 满足要求的三元组集合为：
+ [
+ [-1, 0, 1],
+ [-1, -1, 2]
+ ]
+
+ * @date 2019/9/11 
+ **/
+public class SumOfThreeNum {
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2,10,-14,11,5,-4,2,0,-10,-10,5,7,-11,10,-2,-5,2,12,-5,14,-11,-15,-5,12,0
+                ,13,8,7,10,6,-9,-15,1,14,11,-9,-13,-10,6,-8,-5,-11,6,-9,14,11,-7,-6,8,3,-7,5,-5,3,2,10,-6,
+                -12,3,11,1,1,12,10,-8,0,8,-5,6,-8,-6,8,-12,-14,7,9,12,-15,-12,-2,-4,-4,-12,6,7,-3,-6,-14,
+                -8,4,4,9,-10,-7,-4,-3,1,11,-1,-8,-12,9,7,-9,10,-1,-14,-1,-8,11,12,-5,-7};
+        System.out.println("数字数组为："+ Arrays.toString(nums));
+        System.out.println("满足条件的三元组集合为：" + threeSum_01(nums).toString());
+    }
+
+    /**
+     * 第一种：会超出时间限制，但不考虑时间应该可以通过测试
+     */
+    private static List<List<Integer>> threeSum_01(int[] nums) {
+        List<List<Integer>> ret = new ArrayList<>();
+        Set<String> numStr = new HashSet<>();
+        if (nums == null || nums.length < 3) {
+            return ret;
+        }
+        String temp;
+        int[] tempArr;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    tempArr = new int[]{nums[i], nums[j], nums[k]};
+                    Arrays.sort(tempArr);
+                    temp = Arrays.toString(tempArr);
+                    if (numStr.add(temp) && nums[i] + nums[j] + nums[k] == 0) {
+                        ret.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k])));
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+    
+    // region 抄作业时间
+
+    /**
+     * 第二种方法：左右坐标法
+     */
+    private static List<List<Integer>> threeSum_02(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int len = nums.length;
+        if (len < 3) {
+            return ans;
+        }
+
+        // 1. 排序
+        Arrays.sort(nums);
+
+        // 2. for循环，两端判断
+        for (int i = 0; i < len; i++) {
+            // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+            if (nums[i] > 0) {
+                break;
+            }
+            // 去重
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = len - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    // 去重
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return ans;
+    }
+    
+    // endregion
+
+}
